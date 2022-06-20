9c17c389224fdab08eaae78e337af5f529f3ab10
devCecy <im.ceciliaan@gmail.com>
2022-06-20 23:29:24 +0900

    movie, tv, search api 추가

--- src/api.ts
+++ src/api.ts
@@ -1,15 +1,16 @@
 const API_KEY = "25ea8bc1ae70a7d0eded15fd04bcaa70";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
-interface IMovies {
+export interface IMovies {
   id: number;
   backdrop_path: string;
   poster_path: string;
   title: string;
   overview: string;
+  media_type?: string;
 }
 export interface IGetMoviesResult {
-  dates: {
+  dates?: {
     maximum: string;
     minimum: string;
   };
@@ -18,8 +19,67 @@ export interface IGetMoviesResult {
   total_pages: number;
   total_results: number;
 }
+
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
     (res) => res.json()
   );
+}
+
+export function getTopRatedMovie() {
+  return fetch(
+    `${BASE_PATH}/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+  ).then((res) => res.json());
+}
+
+export function getLatestMovie() {
+  return fetch(`${BASE_PATH}/movie/latest?api_key=${API_KEY}`).then((res) =>
+    res.json()
+  );
+}
+
+export function getUpcommingMovie() {
+  return fetch(`${BASE_PATH}/movie/upcoming?api_key=${API_KEY}`).then((res) =>
+    res.json()
+  );
+}
+
+// tv
+export interface ITv {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  name: string;
+  overview: string;
+}
+
+export interface IGetTvResult {
+  page: number;
+  results: ITv[];
+  total_pages: number;
+  total_results: number;
+}
+export function getOntheAirTv() {
+  return fetch(`${BASE_PATH}/tv/on_the_air?api_key=${API_KEY}`).then((res) =>
+    res.json()
+  );
+}
+
+export function getTopRatedTv() {
+  return fetch(`${BASE_PATH}/tv/top_rated?api_key=${API_KEY}`).then((res) =>
+    res.json()
+  );
+}
+
+export function getPopularTv() {
+  return fetch(`${BASE_PATH}/tv/popular?api_key=${API_KEY}`).then((res) =>
+    res.json()
+  );
+}
+
+// search
+export function getSearchResults(keyword: string) {
+  return fetch(
+    `${BASE_PATH}/search/multi?api_key=${API_KEY}&language=en-US&query=${keyword}&page=1&include_adult=false`
+  ).then((res) => res.json());
 }
